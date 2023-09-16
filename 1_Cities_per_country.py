5fc3353cd8e828eacb9136252a0091da1b74d325
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-16 14:24:55 +0530

    Add files via upload

---
+++ 1_Cities_per_country.py
@@ -0,0 +1,17 @@
+india = ["Mumbai","Kolkatha","HYD","Banglore"]
+USA = ["NYC","WC","arizona","texas","dallas","North carilone"]
+PAK = ["lahore","Jaislamarbad","POK"]
+
+user = input("Enter the city name? : ")
+
+if user in india:
+     print(f'your entered city {user} is present in the india')
+
+elif user in USA:
+    print(f'your entered city {user} is present in the USA')
+
+elif user in PAK:
+    print(f'your entered city da{user} is  present in the PAK')
+
+else :
+    print('your entered city is not present in the List given above')
