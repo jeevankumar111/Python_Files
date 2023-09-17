0f35ed229a7126b0d5300e18b28e23588f9500e5
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-17 23:41:55 +0530

    Add files via upload

---
+++ 4_EXP.py
@@ -0,0 +1,14 @@
+exp = [2000,2200,2500,1500,1200]
+
+print('in feb this muc extra i have spent : ',exp[1]-exp[0])
+
+print('The total epenses of the first 3 months are : ',exp[0]+exp[1]+exp[3])
+
+print('Have  you spent 200 dollars in any month : ', 2000 in exp)
+
+exp.append(1980)
+print('June month expenses has been added : ',exp)
+
+exp[3]=exp[3]+300
+print('200 dollars are returned in the month of Aprial : ',exp)
+
