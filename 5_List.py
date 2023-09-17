01911f77e7cd194ae232de52811ad961fec6b676
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-17 23:42:14 +0530

    Add files via upload

---
+++ 5_List.py
@@ -0,0 +1,16 @@
+Heroes = ["hulk",'captain','thor','natsha','spidy']
+print(len(Heroes))
+
+Heroes.append('Black panther')
+print(Heroes)
+
+Heroes.remove('Black panther')
+Heroes.insert(3,'Black panther')
+print(Heroes)
+
+Heroes[1:3] =['DR strange']
+print(Heroes)
+
+Heroes.sort()
+print(Heroes)
+
