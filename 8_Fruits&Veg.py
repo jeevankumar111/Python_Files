a6ec4b084c38916f817c88c94db1beee88264b9c
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-18 23:12:33 +0530

    Add files via upload

---
+++ 8_Fruits&Veg.py
@@ -0,0 +1,4 @@
+Fruits = ["Mango","Banana","Apple","slice","Dragonfruit","litchi"]
+Veg    = ["Potato","brinjal","califlower"]
+
+print(f'i have eaten{Fruits} and plenty of {Veg} in Italy they are amazing')
