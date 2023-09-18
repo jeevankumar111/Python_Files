87be9390d076f5b270d1a16f9e12b925077793b9
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-18 23:14:25 +0530

    Add files via upload

---
+++ 9_Repalce.py
@@ -0,0 +1,8 @@
+s ="maine 200 banana kaya"
+s=s.replace('200','10')
+s=s.replace('banana','samosa')
+print('After changing the words : ',s)
+
+s ="maine 200 banana kaya"
+s = s.replace("banana","samosa").replace("200","10")
+print("After printing in the same line : ",s)
