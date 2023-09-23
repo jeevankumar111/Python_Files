413b4f797ba06c861a0b83d759b1446f295baf74
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-24 01:33:40 +0530

    Add files via upload

---
+++ 29_Custom_Exceptiom.py
@@ -0,0 +1,24 @@
+class AdultException(Exception):
+    pass
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def get_minor_age(self):
+        if int(self.age) >=18:
+            raise AdultException
+        else:
+
+            return self.age
+
+    def display(self):
+        try:
+            print(f"age -> {self.get_minor_age()}")
+        except AdultException:
+            print("Person is an adult")
+        finally:
+            print(f"name -> {self.name}")
+
+Person("Bhavin",19).display()
