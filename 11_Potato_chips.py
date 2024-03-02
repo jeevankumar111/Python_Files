91b2246e39eb057403a49cb6fb0bd1ef747a9510
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-03-02 19:57:25 +0530

    Update 11_Potato_chips.py

--- 11_Potato_chips.py
+++ 11_Potato_chips.py
@@ -1,7 +1,7 @@
-num_Packets =9
+0num_Packets =9
 Cost_packet = 1.49
 Total_cost = num_Packets*Cost_packet
 Money_Paid=20
 Money_return=Money_Paid-Total_cost
 print("Total Amount to be returned is",Money_return)
-00000000000000000000
+000000000000000000000
