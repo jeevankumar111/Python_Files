5e730858a9fa250f29f31cc1d4f643a70c720359
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-03-07 19:36:38 +0530

    Update 13_convert celcius to forighn heat.py

--- 13_convert celcius to forighn heat.py
+++ 13_convert celcius to forighn heat.py
@@ -7,4 +7,4 @@ celsius = 37.5
 fahrenheit = (celsius * 1.8) + 32
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
 0000000000000000000000000000
-000000
+0000000
