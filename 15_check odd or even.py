07574780eb19cd12c7c946c6f34f49a3a989c1ab
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-03-24 20:23:08 +0530

    Update 15_check odd or even.py

--- 15_check odd or even.py
+++ 15_check odd or even.py
@@ -1,4 +1,4 @@
-000000000000000# Python program to check if the input number is odd or even.
+0000000000000000# Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
