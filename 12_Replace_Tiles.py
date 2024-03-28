3c9e1a96b2fb06946db747bed0d1f7de0229c2a2
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-03-28 20:16:16 +0530

    Update 12_Replace_Tiles.py

--- 12_Replace_Tiles.py
+++ 12_Replace_Tiles.py
@@ -1,4 +1,4 @@
-000000000000000000length=5.5
+0000000000000000000length=5.5
 area=length**2 # area of square is length power 2
 cost=area*500
 print("total cost for bathroom tiles replacement:",cost)
