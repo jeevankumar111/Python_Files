6eb160f6279337c887f930c04689c5c2227bf18e
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-04-08 20:42:42 +0530

    Update 16_Store_Name.py

--- 16_Store_Name.py
+++ 16_Store_Name.py
@@ -1,4 +1,4 @@
-0000000000First_Name = "Jeevan"00000
+00000000000First_Name = "Jeevan"00000
 Middle_Name = "Jyothi"
 Last_Name = "Kumar"
 print("My full name is : "+First_Name+" "+Middle_Name+" "+Last_Name)
